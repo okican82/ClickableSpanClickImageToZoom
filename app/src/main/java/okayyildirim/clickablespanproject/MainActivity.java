106b82e2adb849384da284b388c8141cd1f1fec9
okay yıldırım <okay.yildirim@gmail.com>
2021-09-30 14:33:55 +0300

    webview added

--- app/src/main/java/okayyildirim/clickablespanproject/MainActivity.java
+++ app/src/main/java/okayyildirim/clickablespanproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
 
     private TextView txt_tv;
     private String questionText;
+    private WebView ww;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,9 @@ public class MainActivity extends Activity {
 
 
         Button button = (Button) findViewById(R.id.button);
+        ww = (WebView) findViewById(R.id.ww);
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -28,6 +33,8 @@ public class MainActivity extends Activity {
               public void onClick(View v)
               {
                   System.gc();
+                  ww.loadData(questionText, "text/html; charset=utf-8", "UTF-8");
+
                   Utils.setHtmlText(getApplicationContext(), txt_tv, questionText);
 
                   txt_tv.setMovementMethod(LinkMovementMethod.getInstance());
@@ -38,7 +45,13 @@ public class MainActivity extends Activity {
         txt_tv = (TextView) findViewById(R.id.txt_tv);
 
         //html Text with img url
-        questionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/><img src=\"http://www.kingglitter.com/wp-content/uploads/2015/10/request-a-sample-547-p.jpg\"/><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/><img src=\"http://www.kingglitter.com/wp-content/uploads/2015/10/request-a-sample-547-p.jpg\"/><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/><img src=\"http://www.kingglitter.com/wp-content/uploads/2015/10/request-a-sample-547-p.jpg\"/><br/>";
+        questionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/> " +
+                "<img alt=\"\" src=\"http://test.myenocta.com/mobilemaintain/eep/common_show_picture_cached.aspx?encQS=uQMsFm++hGvvJQvioV4DMyS4z1qn3GJw2LFPzJSQx05nat/0zxCPg0QVtMIQNF7vfyumEdqwNAN3Y8ME+jZLdGqDJejY6mllXT4rtmpz25nFCu6M4padvhysB7aqhH8zknvzQWDtq4+YYD3GGRGQfw==\" " +
+                "style=\"width:458px; height:164px\"><br>\n<br>\n " +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/> " +
+                "<img alt=\"\" src=\"http://test.myenocta.com/mobilemaintain/eep/common_show_picture_cached.aspx?encQS=uQMsFm++hGvvJQvioV4DMyS4z1qn3GJw2LFPzJSQx05nat/0zxCPg0QVtMIQNF7vfyumEdqwNAN3Y8ME+jZLdGqDJejY6mllXT4rtmpz25nFCu6M4padvhysB7aqhH8zknvzQWDtq4+YYD3GGRGQfw==\" " +
+                "style=\"width:458px; height:164px\"><br>\n<br>\n "
+        ;
 
     }
 }
